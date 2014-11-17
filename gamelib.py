a9c42824d48dc409d484d3d51706a5aab14dab3d
dot <chodanun@9-140.kuwin.ku.ac.th>
2014-11-17 14:58:33 +0700

    Second Commit

---
+++ gamelib.py
@@ -0,0 +1,20 @@
+import pygame
+from pygame.locals import *
+
+class SimpleGame(object):
+	def __init__(self):
+		pass
+	
+	def run (self):
+		pass
+
+	def init(self):
+		pass
+
+	def update(self):
+		pass
+
+	def render(self):
+		pass
+
+		
